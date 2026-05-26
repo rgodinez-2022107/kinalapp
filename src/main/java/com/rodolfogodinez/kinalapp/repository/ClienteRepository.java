c78f0ca14a3051b4ca4b306fafd94b53f4eddccc
rgodinez-2022107 <rgodinez-2022107@kinal.edu.gt>
2026-05-25 20:35:59 -0600

    fix: Actualizar tipo de ID de String a Long en ClienteRepository

--- src/main/java/com/rodolfogodinez/kinalapp/repository/ClienteRepository.java
+++ src/main/java/com/rodolfogodinez/kinalapp/repository/ClienteRepository.java
@@ -1,12 +1,9 @@
 package com.rodolfogodinez.kinalapp.repository;
 
-import java.util.List;
 import com.rodolfogodinez.kinalapp.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
-public interface ClienteRepository extends JpaRepository<Cliente,String> {
-
+public interface ClienteRepository extends JpaRepository<Cliente, String> {
     List<Cliente> findByEstado(Integer estado);
-
-
 }
