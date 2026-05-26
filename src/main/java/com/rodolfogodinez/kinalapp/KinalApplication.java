7aff61f888a4f105c92b1b289d44f20d0818e5e1
rgodinez-2022107 <rgodinez-2022107@kinal.edu.gt>
2026-05-25 20:39:27 -0600

    chore: Actualizar configuración principal de la aplicación

--- src/main/java/com/rodolfogodinez/kinalapp/KinalApplication.java
+++ src/main/java/com/rodolfogodinez/kinalapp/KinalApplication.java
@@ -3,13 +3,9 @@ package com.rodolfogodinez.kinalapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
-public class
-KinalApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(KinalApplication.class, args);
-	}
-
+public class KinalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KinalApplication.class, args);
+    }
 }
